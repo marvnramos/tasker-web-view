ea58b7d0a47342d60678974c977d53d49ac36cd8
Marvin Ramos <mr487223@gmail.com>
2024-05-21 13:14:29 -0600

    feat: Add responsive centering for app container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+
+window.addEventListener('resize', () => {
+  const app = document.getElementById('app')
+  app!.style.display = 'flex'
+  app!.style.justifyContent = 'center'
+  app!.style.alignItems = 'center'
+})
 </script>
 <template>
   <RouterView />
