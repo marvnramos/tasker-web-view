5f8eb34fab79888ae7d658cbbfd974f0fa3fc9a0
Marvin Ramos <mr487223@gmail.com>
2024-05-21 15:46:20 -0600

    chore: Update main.ts to import index.css for consistency

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './assets/main.css'
+import './assets/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/index.css'
 
 const app = createApp(App)
 
