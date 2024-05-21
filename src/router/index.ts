ff4520c7838420837e907e424925aed01ae86d83
Marvin Ramos <mr487223@gmail.com>
2024-05-21 15:46:58 -0600

    refactor: Update router configuration to use HomeView for the home route

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,9 @@ const router = createRouter({
       component: AuthView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/home',
+      name: 'Home',
+      component: HomeView
     }
   ]
 })
