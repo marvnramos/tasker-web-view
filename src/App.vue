d93da5265f929d82ceb3a78a95efe48464d17d7b
Marvin Ramos <mr487223@gmail.com>
2024-05-22 14:44:16 -0600

    refactor: Update background styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@ window.addEventListener('resize', () => {
 })
 </script>
 <template>
-    <div id="background" class="sticky top-0 z-0 h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div class="sticky flex items-center justify-center top-0 z-0 h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+    <!-- <div id="background" class="sticky top-0 z-0 h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"> -->
       <RouterView />
     </div>
 </template>
