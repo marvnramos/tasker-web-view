071490ad36d664cc1298a66727825a5f803b5cb4
Marvin Ramos <mr487223@gmail.com>
2024-05-22 10:53:29 -0600

    refactor: Update width size to 50%

--- src/views/User/UserView.vue
+++ src/views/User/UserView.vue
@@ -52,8 +52,8 @@
     <div id="success-alert" class="flex items-center justify-center w-screen mt-10">
     </div>
         <div class="flex items-center justify-center h-screen w-screen">
-        <div class="flex items-center justify-center bg-[#FCFFE0] w-700 rounded-lg">
-            <div class="auth  p-10 bg-[#7ABA78] rounded-l-lg">
+        <div class="flex items-center justify-center bg-[#FCFFE0] w-1/2	 rounded-lg">
+            <div class="auth  p-10 bg-[#7ABA78] rounded-l-lg w-1/2">
                 <form class="max-w-sm mx-auto" @submit.prevent="clickedSubmit()">
                     <div class="mb-5">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
@@ -75,10 +75,10 @@
                         <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repetir Contraseña</label>
                         <input type="password" id="repeat-password" v-model="form.repeatPassword" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#4F6F52] dark:border-[#4F6F52] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                     </div>
-                    <button type="submit" class="text-white bg-[#799351] hover:bg-[#1A4D2E] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#0A6847] dark:hover:bg-blue dark:focus:ring-blue-800">Register new account</button>
+                    <button type="submit" class="text-white bg-[#799351] hover:bg-[#1A4D2E] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#0A6847] dark:hover:bg-blue dark:focus:ring-blue-800">Crear Registro</button>
                 </form>
             </div>
-            <div class=" w-500 m-10">
+            <div class=" w-1/2 flex items-center justify-center">
                 <img alt="Vue logo" class="logo" src="@/assets/register-frog.webp" width="250" height="250" />
             </div>
         </div>
