dee2f35ecbdf2a5e9f5ab586776cd4beb5e9b5bb
Marvin Ramos <mr487223@gmail.com>
2024-05-22 10:58:21 -0600

    refactor: Add "Ya tienes una cuenta" helper text in UserView.vue

--- src/views/User/UserView.vue
+++ src/views/User/UserView.vue
@@ -74,6 +74,8 @@
                         <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repetir Contraseña</label>
                         <input type="password" id="repeat-password" v-model="form.repeatPassword" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#4F6F52] dark:border-[#4F6F52] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                     </div>
+                    <p id="helper-text-explanation" class="mt-2 text-sm text-gray-900 dark:text-white">¿Ya tienes una cuenta? <a href="/" class="font-medium text-blue-600 hover:underline dark:text-[#1A4D2E]">Inicia sesión</a>.</p>
+                    <br/>
                     <button type="submit" class="text-white bg-[#799351] hover:bg-[#1A4D2E] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#0A6847] dark:hover:bg-blue dark:focus:ring-blue-800">Crear Registro</button>
                 </form>
             </div>
