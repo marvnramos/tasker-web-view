fc60338bf05a418fc30472a921a875389baea37c
Marvin Ramos <mr487223@gmail.com>
2024-05-22 10:45:19 -0600

    refactor: Update router configuration to use RegisterView for the register route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AuthView from '@/views/Auth/AuthView.vue'
+import RegisterView from '@/views/User/UserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +11,9 @@ const router = createRouter({
       component: AuthView
     },
     {
-      path: '/home',
-      name: 'Home',
-      component: HomeView
+      path: '/register',
+      name: 'Register',
+      component: RegisterView
     }
   ]
 })
