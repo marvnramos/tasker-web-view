12197ea74f6bb1104643117a84a3c118e70cdb74
Marvin Ramos <mr487223@gmail.com>
2024-05-22 15:51:22 -0600

    refactor: Update Table.vue to improve task display and interaction

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -57,8 +57,8 @@ import type { Task } from '@/utils/task-interface';
 </script>
 
 <template>
-    <div class="absolute overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <div class="fixed overflow-x-auto shadow-md sm:rounded-lg w-screen p-20  flex items-center justify-center">
+        <table class="w-3/5 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
