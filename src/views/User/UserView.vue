6b25c787c7c25723090a278a9bc4a6a5917d4c63
Marvin Ramos <mr487223@gmail.com>
2024-05-23 15:48:35 -0600

    refactor: Add error handling for password mismatch in UserView.vue

--- src/views/User/UserView.vue
+++ src/views/User/UserView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
     import { createApp, reactive } from 'vue';
     import success from '@/components/success.vue';
+    import error from '@/components/UserExists.vue';
 
     const form = reactive({
         name: '',
@@ -17,6 +18,13 @@
             email: form.email,
             password: form.password
         }
+
+        if(form.password !== form.repeatPassword){
+            alert('Las contraseñas no coinciden');
+            return; 
+        }
+
+        // console.log(form);
         
         fetch('http://localhost:3000/users/', {
             method: 'POST',
@@ -26,7 +34,19 @@
             },
             body: JSON.stringify(request_payload)
         }).then(async response => {
-            if (!response.ok) {
+            if(response.status === 400) {
+                const errorAlert = document.getElementById('error-alert');
+                if(errorAlert) {
+                    if(errorAlert.firstChild) {
+                        errorAlert.removeChild(errorAlert.firstChild);
+                    }
+                    createApp(error).mount(errorAlert);
+                    setTimeout(() => {
+                        errorAlert.classList.add('fade-out');
+                    }, 1000);
+                }
+                return;
+            }else if(!response.ok && response.status !== 400) {
                 throw new Error('Invalid email or password');
             }
 
@@ -48,7 +68,67 @@
 
 
 <template>
+    <div id="error-alert" class="flex items-center justify-center w-screen mt-10 absolute"></div> 
     <div id="success-alert" class="flex items-center justify-center w-screen mt-10 absolute">
+    </div>
+   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="@/assets/register-frog.webp" alt="Your Company" />
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Registrarse</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="#" method="POST" @submit.prevent="clickedSubmit">
+            <div class="flex space-x-2">
+                <div class="w-1/2">
+                    <label for="name" class="block text-sm font-medium leading-6 text-white">Nombre</label>
+                    <div class="mt-2">
+                        <input id="name" v-model="form.name" name="name" type="text" required class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <label for="surname" class="block text-sm font-medium leading-6 text-white">Apellido</label>
+                    <div class="mt-2">
+                        <input id="surname" v-model="form.surname" name="surname" type="text" required class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <label for="email" class="block text-sm font-medium leading-6 text-white">Correo electrónico</label>
+                <div class="mt-2">
+                    <input id="email" v-model="form.email" name="email" type="email" required class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+                </div>
+            </div>
+
+            <div>
+                <label for="password" class="block text-sm font-medium leading-6 text-white">Contraseña</label>
+                <div class="mt-2">
+                    <input id="password" v-model="form.password" name="password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+                </div>
+            </div>
+
+            <div>
+                <label for="repeat-password" class="block text-sm font-medium leading-6 text-white">Repetir Contraseña</label>
+                <div class="mt-2">
+                    <input id="repeat-password" v-model="form.repeatPassword" name="repeat-password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+                </div>
+            </div>
+
+            <div>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Crear cuenta</button>
+            </div>
+        </form>
+
+        <p class="mt-10 text-center text-sm text-gray-400">
+            ¿Ya tienes una cuenta?
+            <a href="/" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Inicia sesión</a>
+        </p>
+    </div>
+</div>
+
+
+    <!-- <div id="success-alert" class="flex items-center justify-center w-screen mt-10 absolute">
     </div>
         <div class="flex items-center justify-center h-screen w-screen">
         <div class="flex items-center justify-center rounded-lg w-1/2 bg-[#ACE1AF] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] z-10">
@@ -83,7 +163,7 @@
                 <img alt="Vue logo" class="logo" src="@/assets/register-frog.webp" width="250" height="250" />
             </div>
         </div>
-        </div>
+        </div> -->
  </template>
 
  <style>
