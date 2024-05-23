5b60a86ef31af10ddec3fa26d4862c3733211ee9
Marvin Ramos <mr487223@gmail.com>
2024-05-23 15:55:17 -0600

    refactor: Update StoreTaskModal.vue to add form for creating tasks

--- src/components/StoreTaskModal.vue
+++ src/components/StoreTaskModal.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
 import { defineEmits } from 'vue';
+import { ref } from 'vue';
 
 const emits = defineEmits(['closeModal']);
 
 const openModal = () => {
     emits('closeModal');
 };
+
+const form = ref({
+    name: '',
+    description: '',
+    category: '',
+    status: ''
+});
+
+
+
+function createTask() {
+
+    const request_payload = {
+    name: form.value.name,
+    description: form.value.description,
+    category: form.value.category,
+    status: form.value.status,
+    // userId: localStorage.getItem('token') // desencriptar token
+    
+    };
+    fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request_payload)
+    })
+}
+
+
 </script>
 <template>
 <!-- Main modal -->
@@ -27,11 +58,11 @@ const openModal = () => {
                 </button>
             </div>
             <!-- Modal body -->
-            <form class="p-4 md:p-5">
+            <form class="p-4 md:p-5" >
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required>
+                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escribe el titulo de la tarea aquí" required>
                     </div>
                     <div class="col-span-2">
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
@@ -40,9 +71,9 @@ const openModal = () => {
                     <div class="col-span-2 sm:col-span-1">
                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prioridad</label>
                         <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option value="TV">Alta</option>
+                            <option value="TV">Baja</option>
                             <option value="PC">Media</option>
-                            <option value="GA">Baja</option>
+                            <option value="GA">Alta</option>
                         </select>
                     </div>
                     <div class="col-span-2 sm:col-span-1">
@@ -56,7 +87,7 @@ const openModal = () => {
                 </div>
                 <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                    Add new product
+                    Agregar nueva tarea
                 </button>
             </form>
         </div>
