8c6db8e13f985ab2f4ae371631a42ee20ab6054d
Marvin Ramos <mr487223@gmail.com>
2024-05-23 15:48:16 -0600

    refactor: Update AuthView to use async/await for login request

--- src/views/Auth/AuthView.vue
+++ src/views/Auth/AuthView.vue
@@ -36,42 +36,42 @@
 
 <template>
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="@/assets/login-frog.webp" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Iniciar sesión</h2>
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="@/assets/login-frog.webp" alt="Your Company" />
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Iniciar sesión</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="#" method="POST" @submit.prevent="handleClick">
+            <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-white">Correo Electrónico</label>
+            <div class="mt-2">
+                <input id="email" v-model="form.email" name="email" type="email" autocomplete="email" required class="block w-full text-white rounded-md border-0 py-1.5 shadow-sm ring-gray-700 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+            </div>
+            </div>
+
+            <div>
+            <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm font-medium leading-6 text-white">Contraseña</label>
+            </div>
+            <div class="mt-2">
+                <input id="password" v-model="form.password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-gray-700 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
+            </div>
+
+            </div>
+
+            <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            </div>
+        </form>
+
+        <p class="mt-10 text-center text-sm text-gray-400">
+            ¿No tienes una cuenta?
+            {{ ' ' }}
+            <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Regístrate</a>
+        </p>
+        </div>
     </div>
-
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST" @submit.prevent="handleClick">
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-white">Correo Electrónico</label>
-          <div class="mt-2">
-            <input id="email" v-model="form.email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-gray-700 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-white">Contraseña</label>
-          </div>
-          <div class="mt-2">
-            <input id="password" v-model="form.password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-gray-700 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-700" />
-        </div>
-
-        </div>
-
-        <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-        </div>
-      </form>
-
-      <p class="mt-10 text-center text-sm text-gray-400">
-        ¿No tienes una cuenta?
-        {{ ' ' }}
-        <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Regístrate</a>
-      </p>
-    </div>
-  </div>
     <!-- <div class="flex items-center justify-center h-screen w-screen">
         <div class="flex items-center justify-center rounded-lg w-1/2 bg-[#ACE1AF] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] z-10">
             <div class="w-1/2 flex items-center justify-center">
