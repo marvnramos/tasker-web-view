dd9a06a915d4124c88568ec8bbc72c088df80194
Marvin Ramos <mr487223@gmail.com>
2024-05-24 15:26:48 -0600

    refactor: Update DeleteTaskModal.vue to handle task deletion and improve modal behavior

--- src/components/DeleteTaskModal.vue
+++ src/components/DeleteTaskModal.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
-import {  defineEmits, onMounted } from 'vue';
+import {  defineEmits, defineProps } from 'vue';
 
 const emits = defineEmits(['closeModal']);
+const props = defineProps<{ taskId: string | null }>();
+
 const openModal = () => {
     emits('closeModal');
 };
 
-onMounted(() => {
-    console.log('aqui va la llamada al backend');
-});
+function handleClic(){
+    fetch(`http://localhost:8080/tasks/delete/${props.taskId}`, {
+    method: 'DELETE',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        // 'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+    }).then(
+        response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener las tareas');
+            }
+            return response.json();
+        }
+    ).then(data => {
+        console.log(data);
+        openModal();
+    }).catch(error => {
+        console.error(error);
+    });
+}
 
 </script>
 <template>
@@ -27,7 +48,8 @@ onMounted(() => {
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                     </svg>
                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                    <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                    <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                    @click.prevent="handleClic">
                         Yes, I'm sure
                     </button>
                     <button type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
