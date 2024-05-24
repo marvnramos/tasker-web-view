9d74c5043e650302b6a36deb850b54d62f8a6a45
Marvin Ramos <mr487223@gmail.com>
2024-05-24 15:10:31 -0600

    refactor: Add delete task functionality to Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,20 +3,32 @@
 import {ref, onMounted, defineEmits} from 'vue';
 import type { Task } from '@/utils/task-interface';
 import EditTask from '@/components/EditTaskModal.vue';
+import DelTask from '@/components/DeleteTaskModal.vue';
 
     let tasks = ref<Task[]>([]);
     const selectedTaskId = ref<string | null>(null);
 
     const emits = defineEmits(['closeModal']);
+
+    const showDelModal = ref(false);
     const showModal = ref(false);
 
     const openModal = (taskId: string) => {
         selectedTaskId.value = taskId;
         showModal.value = true;
     };
+    const openDelModal = (taskId: string) => {
+        selectedTaskId.value = taskId;
+        showDelModal.value = true;
+    };
 
     const closeModal = () => {
         showModal.value = false;
+        selectedTaskId.value = null;
+        emits('closeModal');
+    };
+    const closeDelModal = () => {
+        showDelModal.value = false;
         selectedTaskId.value = null;
         emits('closeModal');
     };
@@ -115,7 +127,8 @@ import EditTask from '@/components/EditTaskModal.vue';
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                             </svg>
                         </button>
-                        <button id="delete" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                        <button id="delete" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        @click.prevent="openDelModal(task.id)">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
@@ -125,6 +138,7 @@ import EditTask from '@/components/EditTaskModal.vue';
             </tbody>
         </table>
     </div>
+    <DelTask :taskId="selectedTaskId" v-if="showDelModal" @close-modal="closeDelModal"/>
     <EditTask :taskId="selectedTaskId" v-if="showModal" @close-modal="closeModal"/>
 </template>
 
