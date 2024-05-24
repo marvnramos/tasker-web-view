5a599fbec87c82ce36b7ad7c25bc00eb4fd9a7ad
Marvin Ramos <mr487223@gmail.com>
2024-05-24 10:24:11 -0600

    refactor: Update StoreTaskModal.vue to add form for creating tasks

--- src/components/StoreTaskModal.vue
+++ src/components/StoreTaskModal.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import router from '@/router';
 import { defineEmits } from 'vue';
 import { ref } from 'vue';
 
@@ -9,31 +10,45 @@ const openModal = () => {
 };
 
 const form = ref({
-    name: '',
+    title: '',
     description: '',
-    category: '',
+    priority: '',
     status: ''
 });
 
 
 
 function createTask() {
-
     const request_payload = {
-    name: form.value.name,
+    title: form.value.title,
     description: form.value.description,
-    category: form.value.category,
+    priority: form.value.priority,
     status: form.value.status,
+    userId: "664e21381bff24656d65fdaf"
     // userId: localStorage.getItem('token') // desencriptar token
-    
     };
-    fetch('http://localhost:3000/tasks', {
+
+    fetch('http://localhost:8080/tasks/create', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
+        // add auth bearer token
         body: JSON.stringify(request_payload)
-    })
+    }).then(
+        response => {
+            if (!response.ok) {
+                throw new Error('Error al crear la tarea');
+            }
+            return response.json();
+        }
+    ).then(data => {
+        console.log(data);
+        openModal();
+        router.push('/tasks');
+    }).catch(error => {
+        console.error(error);
+    });
 }
 
 
@@ -58,34 +73,35 @@ function createTask() {
                 </button>
             </div>
             <!-- Modal body -->
-            <form class="p-4 md:p-5" >
+            <form class="p-4 md:p-5" method="POST" @submit.prevent="createTask" >
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escribe el titulo de la tarea aquí" required>
+                        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
+                        <input type="text" v-model="form.title" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escribe el titulo de la tarea aquí" required>
                     </div>
                     <div class="col-span-2">
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
-                        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escribe la descripción de la tarea aquí"></textarea>                    
+                        <textarea id="description" v-model="form.description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escribe la descripción de la tarea aquí"></textarea>                    
                     </div>
                     <div class="col-span-2 sm:col-span-1">
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prioridad</label>
-                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option value="TV">Baja</option>
-                            <option value="PC">Media</option>
-                            <option value="GA">Alta</option>
+                        <label for="priority" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prioridad</label>
+                        <select id="priority" v-model="form.priority" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="Baja">Baja</option>
+                            <option value="Media">Media</option>
+                            <option value="Alta">Alta</option>
                         </select>
                     </div>
                     <div class="col-span-2 sm:col-span-1">
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Estado</label>
-                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option value="TV">Por hacer</option>
-                            <option value="PC">En progreso</option>
-                            <option value="GA">Hecho</option>
+                        <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Estado</label>
+                        <select id="status" v-model="form.status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="Por hacer">Por hacer</option>
+                            <option value="En progreso">En progreso</option>
+                            <option value="Hecho">Hecho</option>
                         </select>
                     </div>
                 </div>
-                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                @click.prevent="createTask">
                     <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                     Agregar nueva tarea
                 </button>
