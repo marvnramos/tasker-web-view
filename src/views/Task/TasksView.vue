8ba8b881b497c600091a669438919dc182372555
Marvin Ramos <mr487223@gmail.com>
2024-05-27 12:23:13 -0600

    feat: Add token check in TasksView.vue to redirect to '/not-found' if user is not authenticated

--- src/views/Task/TasksView.vue
+++ src/views/Task/TasksView.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
     import NavMenu from '@/components/NavMenu.vue';
     import Table from '@/components/Table.vue';
+    import router from '@/router';
+
+    const token = localStorage.getItem('token');
+    if (!token) {
+        router.push('/not-found');
+    }
 </script>
 <template>
     <div class="absolute flex items-center justify-center h-screen w-screen">
